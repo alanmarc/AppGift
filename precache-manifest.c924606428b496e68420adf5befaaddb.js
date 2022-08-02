63d42828fe3a4120ed91faecefda0de8e5687943
alanMar <alan.marcos@smartkode.mx>
2022-08-02 10:28:35 -0500

    Updates

---
+++ precache-manifest.c924606428b496e68420adf5befaaddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb30b5d5b9efad27f61a2d64001035c",
+    "url": "/AppGift/index.html"
+  },
+  {
+    "revision": "7cff73492d59794fc031",
+    "url": "/AppGift/static/css/main.1e96e12b.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/AppGift/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AppGift/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cff73492d59794fc031",
+    "url": "/AppGift/static/js/main.883cf5c8.chunk.js"
+  },
+  {
+    "revision": "a3a27a0cd83f381c6d27",
+    "url": "/AppGift/static/js/runtime-main.729203d5.js"
+  }
+]);
